f8f8d2ff44a0fa32840c9a52c6456e49b64d859b
mnbi <mnbi@users.noreply.github.com>
2022-12-08 15:21:19 +0900

    add code to call command

--- cat/cat.go
+++ cat/cat.go
@@ -1,12 +1,26 @@
 package cat
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
 )
 
 func Cat(pathname string) error {
 	if pathname == "" {
 		return errors.New("empty pathname")
+	}
+
+	f, err := os.Open(pathname)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 
 	return nil
--- cmd/gostudy/main.go
+++ cmd/gostudy/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/mnbi/gostudy"
 	"github.com/mnbi/gostudy/cat"
+	"os"
 )
 
 func greeting() string {
@@ -11,11 +12,42 @@ func greeting() string {
 }
 
 func main() {
-	fmt.Printf("gostudy %v\n", gostudy.Version())
 
-	msg := greeting()
-	fmt.Println(msg)
+	cmd, args := parseArgs(os.Args)
 
-	pathname := ""
-	_ = cat.Cat(pathname)
+	switch cmd {
+	case "cat":
+		fmt.Println("***")
+
+		for i := 0; i < len(args); i++ {
+			_ = cat.Cat(args[i])
+			fmt.Println("---")
+		}
+
+		fmt.Println("***")
+	case "greeting":
+		msg := greeting()
+		fmt.Println(msg)
+	case "version":
+		fmt.Printf("gostudy %v\n", gostudy.Version())
+	case "help":
+		appName := os.Args[0]
+		fmt.Printf("%s [command] [args...]\n", appName)
+		fmt.Println("command:")
+		fmt.Println("  cat")
+		fmt.Println("  greeting")
+		fmt.Println("  version")
+		fmt.Println("  help")
+	default:
+		fmt.Println("unknown command")
+	}
+
+}
+
+func parseArgs(args []string) (string, []string) {
+	if len(args) <= 1 {
+		return "", args
+	}
+
+	return args[1], args[2:]
 }
